5a3965fda22bd0165f57031d49000a56f6bdcbce
Juan Louis Rombetasik <13519075@std.stei.itb.ac.id>
2022-02-12 03:42:25 +0000

    Resolve "Membuat Sidebar Navigation UI" #4

---
+++ lib/widgets/sidebar.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class SideNavigationBar extends StatelessWidget {
+  const SideNavigationBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          UserAccountsDrawerHeader(
+            accountName: const Text("John Doe"),
+            accountEmail: const Text("Johndoe@example.com"),
+            currentAccountPicture: CircleAvatar(
+              child: ClipOval(
+                child: Image.network(
+                  "https://lh3.googleusercontent.com/ogw/ADea4I59lb97jZRtDffSpVpk2RXi52W5S0Y3cXq8VIrkCQ=s83-c-mo",
+                  width: 90,
+                  height: 90,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.notifications_outlined),
+            title: const Text("Notifications"),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.collections_bookmark_outlined),
+            title: const Text("Reccomendation"),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.search_outlined),
+            title: const Text("Explore"),
+            onTap: () {},
+          ),
+          const Divider(),
+          ListTile(
+            leading: const Icon(Icons.settings_outlined),
+            title: const Text("Settings"),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.help_outline),
+            title: const Text("Help & Suggestion"),
+            onTap: () {},
+          ),
+          const Divider(),
+          ListTile(
+            leading: const Icon(Icons.exit_to_app),
+            title: const Text("Logout"),
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
