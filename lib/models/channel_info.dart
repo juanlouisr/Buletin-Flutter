355fc5e99f52267ca98b354ba586e50afa8b9cc2
mizuday <mizuday5.7@gmail.com>
2022-02-23 00:49:18 +0700

    fix: fix channel model adjust to latest api

--- lib/models/channel_info.dart
+++ lib/models/channel_info.dart
@@ -1,6 +1,5 @@
 class ChannelInfo {
   final int channelId;
-  final int categoryId;
   final int ownerId;
   final String channelName;
   final String channelPicture;
@@ -8,7 +7,6 @@ class ChannelInfo {
 
   ChannelInfo({
     required this.channelId,
-    required this.categoryId,
     required this.ownerId,
     required this.channelName,
     required this.channelPicture,
@@ -17,7 +15,6 @@ class ChannelInfo {
 
   factory ChannelInfo.fromMap(Map<String, dynamic> map) {
     int channelId = map['channel_id'] as int;
-    int categoryId = map['category_id'] as int;
     int ownerId = map['owner_id'] as int;
     String channelName = map['channel_name'] as String;
     String channelPicture = map['channel_picture'] as String;
@@ -25,7 +22,6 @@ class ChannelInfo {
 
     return ChannelInfo(
       channelId: channelId,
-      categoryId: categoryId,
       ownerId: ownerId,
       channelName: channelName,
       channelPicture: channelPicture,
