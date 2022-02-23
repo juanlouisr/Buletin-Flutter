f74e087374e560a93c1128b80874ebb3182dae21
mizuday <mizuday5.7@gmail.com>
2022-02-23 18:25:01 +0700

    feat: detail playlist

--- lib/widgets/playlist/playlist_card.dart
+++ lib/widgets/playlist/playlist_card.dart
@@ -1,6 +1,7 @@
 import 'package:buletin/constants.dart';
 import 'package:buletin/models/playlist_info.dart';
 import 'package:buletin/widgets/other/aspect_ratio_image.dart';
+import 'package:buletin/widgets/playlist/playlist_detail.dart';
 import 'package:flutter/material.dart';
 
 class PlaylistCard extends StatelessWidget {
@@ -21,36 +22,45 @@ class PlaylistCard extends StatelessWidget {
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(cardBorderRadius),
           ),
-          child: Stack(
-            children: [
-              AspectRatioImageNetwork(
-                image: playlistInfo.thumbnail,
-                aspectRatio: 2.2,
-              ),
-              Positioned(
-                top: 0,
-                left: 0,
-                bottom: 0,
-                child: Container(
-                  // height: 100,
-                  padding: const EdgeInsets.all(8.0),
-                  width: 60,
-                  color: const Color.fromRGBO(0, 0, 0, 0.6),
-                  child: Center(
-                    child: Text(
-                      // playlistInfo.videoList.length.toString(),
-                      playlistInfo.videos.length.toString() + ' videos',
-                      maxLines: 2,
-                      textAlign: TextAlign.center,
-                      style: Theme.of(context)
-                          .textTheme
-                          .caption!
-                          .copyWith(color: Colors.white),
+          child: InkWell(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          PlaylistDetail(playlistInfo: playlistInfo)));
+            },
+            child: Stack(
+              children: [
+                AspectRatioImageNetwork(
+                  image: playlistInfo.thumbnail,
+                  aspectRatio: 2.2,
+                ),
+                Positioned(
+                  top: 0,
+                  left: 0,
+                  bottom: 0,
+                  child: Container(
+                    // height: 100,
+                    padding: const EdgeInsets.all(8.0),
+                    width: 60,
+                    color: const Color.fromRGBO(0, 0, 0, 0.6),
+                    child: Center(
+                      child: Text(
+                        // playlistInfo.videoList.length.toString(),
+                        playlistInfo.videos.length.toString() + ' videos',
+                        maxLines: 2,
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context)
+                            .textTheme
+                            .caption!
+                            .copyWith(color: Colors.white),
+                      ),
                     ),
                   ),
-                ),
-              )
-            ],
+                )
+              ],
+            ),
           ),
         ),
         Padding(
@@ -59,8 +69,7 @@ class PlaylistCard extends StatelessWidget {
             playlistInfo.playlistName,
             maxLines: 1,
             overflow: TextOverflow.ellipsis,
-            style: Theme.of(context)
-                .textTheme.titleLarge,
+            style: Theme.of(context).textTheme.titleLarge,
           ),
         ),
       ],
