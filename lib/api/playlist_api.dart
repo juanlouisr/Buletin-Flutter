9fd4c85d25355cb3357f679d7b44eaf4402571eb
mizuday <mizuday5.7@gmail.com>
2022-03-30 22:26:08 +0700

    refactor: refactor video api

--- lib/api/playlist_api.dart
+++ lib/api/playlist_api.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:buletin/api/video_api.dart';
 import 'package:buletin/constants.dart';
 import 'package:buletin/models/playlist_info.dart';
@@ -12,19 +11,23 @@ class PlaylistAPI {
     };
     var uri = Uri.http(baseUrl, playlistEndpoint, queryParams);
     var response = await http.get(uri);
-    print(response.statusCode);
     var jsonData = jsonDecode(response.body);
 
     List<PlaylistInfo> playlist = [];
 
     for (var pl in jsonData['data']['playlists']) {
-      var playlistId = pl['playlist_id'] as int;
-      var playlistName = pl['playlist_name'] as String;
-      var videoList = await VideoAPI.getPlaylistVideos(playlistId);
-      playlist.add(PlaylistInfo(
+      final playlistId = pl['playlist_id'] as int;
+      final playlistName = pl['playlist_name'] as String;
+      final playlistPicture = pictureParser(pl['playlist_picture']);
+      final videoList = await VideoAPI.getVideos(
+        pageNo: 1,
+        pageSize: 1000,
         playlistId: playlistId,
-        playlistName: playlistName,
-        thumbnail: "https://akcdn.detik.net.id/community/media/visual/2018/10/20/cd7922fb-43a9-4e5f-bb86-af2b820d43d4_169.png?w=700&q=90",
+      );
+      playlist.add(PlaylistInfo(
+        id: playlistId,
+        name: playlistName,
+        picture: playlistPicture,
         videos: videoList,
       ));
     }
